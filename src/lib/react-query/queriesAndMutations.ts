699bbd764ae9bcb0bfae222ca372b799c43f2ac0
Sara Garcia <saragca81@gmail.com>
2024-03-20 15:56:10 -0700

    updated files

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,8 @@ useMutation,
 useQueryClient,
 useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts,  signInAccount, updatePost } from '../appwrite/api'
-import { INewPost, INewUser, IUpdatePost } from '@/types'
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signOutAccount,signInAccount, updatePost, getUsers, getUserPosts, getUserById, updateUser } from '../appwrite/api'
+import { INewPost, INewUser, IUpdatePost, IUpdateUser } from '@/types'
 import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateAccount = () => {
@@ -119,6 +119,13 @@ export const useCreatePost = () => {
         enabled: !!postId
     })
   }
+  export const useGetUserPosts = (userId?: string) => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_USER_POSTS, userId],
+      queryFn: () => getUserPosts(userId),
+      enabled: !!userId,
+    });
+  };
   export const useUpdatePost = () => {
     const queryClient = useQueryClient();
 
@@ -163,3 +170,30 @@ export const useCreatePost = () => {
         enabled: !!searchTerm
     })
   }
+  export const useGetUsers = (limit?: number) => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_USERS],
+      queryFn: () => useGetUsers(limit),
+    });
+  };
+  export const useGetUserById = (userId: string) => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_USER_BY_ID, userId],
+      queryFn: () => getUserById(userId),
+      enabled: !!userId,
+    });
+  };
+  export const useUpdateUser = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (user: IUpdateUser) => updateUser(user),
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_USER_BY_ID, data?.$id],
+        });
+      },
+    });
+  };
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-
+import React, { StrictMode } from "react";
 import  ReactDOM  from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -7,6 +7,7 @@ import AuthProvider from './context/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
     <BrowserRouter>
      <QueryProvider>
         <AuthProvider>
@@ -14,4 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </AuthProvider>   
      </QueryProvider>     
     </BrowserRouter>
+    </React.StrictMode>
 );
